6d074215e749bf51a61bf84b765c2041785be61b
Mike <70263671+winiar93@users.noreply.github.com>
2022-01-03 01:37:48 +0100

    Add files via upload
    upload project files

---
+++ main.py
@@ -0,0 +1,54 @@
+from machine import I2C, Pin
+from HTU21D import HTU21D
+import machine
+import network 
+import urequests
+import time
+
+WIFI_SSID = '*'
+WIFI_PASS = '*'
+HTTP_HEADERS = {'Content-Type': 'application/json'} 
+THINGSPEAK_WRITE_API_KEY = '*'
+
+nic = network.WLAN(network.STA_IF)
+nic.active(True)
+time.sleep(0.5)
+nic.connect(WIFI_SSID, WIFI_PASS)
+while not nic.isconnected():
+    machine.idle()
+    
+
+sensor_data = HTU21D(22,21)
+
+def print_and_send_data_in_loop(interval):
+    while True:
+        hum = sensor_data.humidity
+        temp = sensor_data.temperature
+        print('hum: ', + hum)
+        print('tmp: ', + temp)
+        request = urequests.post('http://api.thingspeak.com/update?api_key=' + THINGSPEAK_WRITE_API_KEY , 
+        json = {'field6': round(temp, 2),
+                'field7': int(hum)}, 
+        headers = HTTP_HEADERS )
+        request.close()
+        time.sleep(interval)
+    
+    
+def send_data_to_thingspeak():
+    hum = sensor_data.humidity
+    temp = sensor_data.temperature
+    request = urequests.post('http://api.thingspeak.com/update?api_key=' + THINGSPEAK_WRITE_API_KEY , 
+    json = {'field6': round(temp, 2),
+            'field7': int(hum)}, 
+    headers = HTTP_HEADERS )
+    print(temp, hum)
+    request.close()
+    nic.active(False)
+    
+def go_deep_sleep(interval: int):
+    # milliseconds 10000 milliseconds = 10sec
+    machine.deepsleep(interval)
+     
+send_data_to_thingspeak()
+go_deep_sleep(1800000)
+    
